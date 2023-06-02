7295ea8f41390481d0880e2006af89fded2dc400
UmarMukhtiar <mumarmukhtiar@gmail.com>
2023-06-02 15:38:03 +0500

    initial commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,54 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    try {
+      //generate password hash
+      const hash = await argon.hash(dto.password);
+      //save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+      delete user.hash;
+      //return saved user
+      return user;
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ForbiddenException('Credentials taken!');
+      }
+
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials incorrect!');
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches)
+      throw new ForbiddenException('Credentials incorrect!');
+
+    delete user.hash;
+
+    return user;
+  }
+}
